e427cc05d89a83b4c2493e6df42ec26539a35e62
Stefan Graupner <stefan.graupner@gmail.com>
2016-03-29 16:40:11 -0400

    Applied fixes from StyleCI

--- src/DeployServiceProvider.php
+++ src/DeployServiceProvider.php
@@ -8,12 +8,12 @@ class DeployServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/laraveldeploy.php', 'laraveldeploy');
+        $this->mergeConfigFrom(__DIR__.'/../config/laraveldeploy.php', 'laraveldeploy');
     }
 
     public function register()
     {
         $this->commands([DeployCommand::class]);
-        $this->publishes([__DIR__ . '/../config/laraveldeploy.php'], 'config');
+        $this->publishes([__DIR__.'/../config/laraveldeploy.php'], 'config');
     }
 }
